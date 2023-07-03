9ecbfe17a5c934b3b30397a556a7153aa04de104
beeplaw <bsc19108@cab.edu.np>
2023-07-03 15:42:22 +0545

    Added checkout feature

--- src/main/java/com/ecommerce/aafrincosmetics/controller/HomePageController.java
+++ src/main/java/com/ecommerce/aafrincosmetics/controller/HomePageController.java
@@ -1,14 +1,17 @@
 package com.ecommerce.aafrincosmetics.controller;
 
 import com.ecommerce.aafrincosmetics.dto.request.CartRequestDto;
-import com.ecommerce.aafrincosmetics.service.CategoryService;
+import com.ecommerce.aafrincosmetics.dto.request.ShipmentRequestDto;
+import com.ecommerce.aafrincosmetics.dto.response.CartResponseDto;
+import com.ecommerce.aafrincosmetics.service.*;
 import com.ecommerce.aafrincosmetics.service.Others.MiscService;
-import com.ecommerce.aafrincosmetics.service.WishlistService;
-import com.ecommerce.aafrincosmetics.service.ProductsService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,9 @@ public class HomePageController {
 
     private final WishlistService wishlistService;
     private final MiscService miscService;
+
+    private final ShipmentService shipmentService;
+    private final CartService cartService;
 
 
     //Getting the Homepage
@@ -39,6 +45,47 @@ public class HomePageController {
     }
 
     //Mapping for checkout
+    @GetMapping("/checkout")
+    public String getCheckoutPage(Model model){
+        if(miscService.isUserLoggedIn()){
+            model.addAttribute("shipmentDetails", shipmentService.getAllShipmentDetails());
+            //Getting all the cart items
+            List<CartResponseDto> allCartItems =  cartService.getAllCartItemsOfUser();
+
+            Integer total=0;
+
+            for(CartResponseDto each: allCartItems){
+                total += each.getQuantity() * each.getProducts().getPrice();
+            }
+
+//            model.addAttribute("cartItems",allCartItems);
+            model.addAttribute("totalPrice", total);
+            return "demo/checkout";
+        }else{
+            return "redirect:/login";
+        }
+    }
+
+    @GetMapping("/get-add-address-form")
+    public String getAddAddressForm(Model model){
+        model.addAttribute("detail", new ShipmentRequestDto());
+        return "demo/detailsForm";
+    }
+
+    @PostMapping("/get-add-address-form")
+    public String saveData(@ModelAttribute ShipmentRequestDto dto){
+        shipmentService.saveShipmentToTable(dto);
+        return "redirect:/checkout";
+    }
+
+    //Delete the shipment address
+//    Place this delete somewhere like in add form
+    @GetMapping("/delete-shipping-address/{id}")
+    public String deleteTheAddress(@PathVariable("id") Integer id){
+        shipmentService.deleteShipmentDetails(id);
+        return "redirect:/checkout";
+    }
+
 
 
 }
